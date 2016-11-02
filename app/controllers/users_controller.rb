62c8dd6efbd58d881d2939a2943f91b4068c1e42
Ashutosh <ashu@listup.co.in>
2016-11-02 20:28:44 +0530

    adding microposts and profile pic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     redirect_to root_url unless @user.activated?
   end
 
@@ -48,21 +49,14 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-  end
-
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
+                                 :password_confirmation,:profile_pic)
   end
 
   def authorized_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
+
   def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102122254) do
+ActiveRecord::Schema.define(version: 20161102143112) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +34,7 @@ ActiveRecord::Schema.define(version: 20161102122254) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.string   "profile_pic"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
